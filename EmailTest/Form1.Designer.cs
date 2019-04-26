d3baf02e8ce6572cbc4bfe0c7e381fdda5ed9cef
Atikur Rahman Sabuj <sabuj.kucse@gmail.com>
2019-04-26 19:03:25 +0600

    First Commit

--- EmailTest/Form1.Designer.cs
+++ EmailTest/Form1.Designer.cs
@@ -28,13 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnSendMail = new System.Windows.Forms.Button();
+            this.tbxMailAddress = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSendMail
+            // 
+            this.btnSendMail.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSendMail.Location = new System.Drawing.Point(371, 125);
+            this.btnSendMail.Name = "btnSendMail";
+            this.btnSendMail.Size = new System.Drawing.Size(138, 54);
+            this.btnSendMail.TabIndex = 0;
+            this.btnSendMail.Text = "SendMail";
+            this.btnSendMail.UseVisualStyleBackColor = true;
+            this.btnSendMail.Click += new System.EventHandler(this.btnSendMail_Click);
+            // 
+            // tbxMailAddress
+            // 
+            this.tbxMailAddress.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbxMailAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxMailAddress.Location = new System.Drawing.Point(32, 133);
+            this.tbxMailAddress.MaximumSize = new System.Drawing.Size(500, 100);
+            this.tbxMailAddress.Name = "tbxMailAddress";
+            this.tbxMailAddress.Size = new System.Drawing.Size(303, 35);
+            this.tbxMailAddress.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(584, 353);
+            this.Controls.Add(this.tbxMailAddress);
+            this.Controls.Add(this.btnSendMail);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnSendMail;
+        private System.Windows.Forms.TextBox tbxMailAddress;
     }
 }
 
--- EmailTest/Form1.cs
+++ EmailTest/Form1.cs
@@ -4,6 +4,8 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +17,37 @@ namespace EmailTest
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnSendMail_Click(object sender, EventArgs e)
+        {
+            if (tbxMailAddress.Text == "")
+            {
+                MessageBox.Show("Enter mail");
+            }
+            string ToMail = tbxMailAddress.Text;
+            string FromMail = "info@dealsdeals.co.uk";
+            SmtpClient client = new SmtpClient();
+            client.UseDefaultCredentials = false;
+            client.Credentials = new NetworkCredential("info@dealsdeals.co.uk", "Laxmi2121");
+            client.Port = 2525;
+            client.Host = "mail.dealsdeals.co.uk";
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.EnableSsl = false;
+            MailAddress From = new MailAddress(FromMail, "DealsDeals");
+            MailAddress To = new MailAddress(ToMail, "Test");
+            MailMessage mailMessage = new MailMessage(From, To);
+            mailMessage.Subject = "Test of mail sending";
+            mailMessage.Body = "Hello dear, I hope you are fine. This a test of our mail and it succeeded, hurrahh!!!.";
+            try
+            {
+                client.Send(mailMessage);
+                MessageBox.Show("Mail send successful");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Could not send mail" + "Error: " + ex.Message);
+            }
         }
     }
 }
